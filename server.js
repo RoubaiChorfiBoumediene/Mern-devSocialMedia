5842027e4d7166e68d93b149bf10bcb19928e3e9
Roubai chorfi boumediene <b.roubaichorfi@esi-sba.dz>
2019-12-17 23:05:59 +0100

    lunching server and testing it on post

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express'); 
+const app = express();
+app.get('/',(req, res)=>res.send('API RUNNING'))
+
+const PORT = process.env.port || 5000 ; 
+
+app.listen(PORT,()=> console.log('server started on port '+ PORT));
